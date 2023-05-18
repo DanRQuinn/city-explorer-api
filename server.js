61b63a9077ec4c3cc5d5e6d0b0e40be96bed73a0
Daniel Quinn <dandidit.quinn@gmail.com>
2023-05-17 19:29:03 -0700

    weatherlive

--- server.js
+++ server.js
@@ -1,68 +1,120 @@
-'use strict';
+import React from 'react';
+import axios from 'axios';
+import { Card } from 'react-bootstrap';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Button from 'react-bootstrap/Button'
+import Weather from './Weather.js'
 
-console.log('our first server');
-
-
-
-//  REQUIRE
-
-const express = require('express');
-require('dotenv').config();
-let data = require('./data/weather.json');
-const cors = require('cors');
-
-//USE
-const app = express();
-app.use(cors());
-
-
-//  PORT
-const PORT = process.env.PORT || 3002;
-
-// ROUTES
-
-app.get('/', (request, response) => {
-  response.send('hello');
-});
-
-app.get('/sayHello', (request, response) => {
-  console.log('hi');
-  console.log(request.query.firstName);
-  let firstName = request.query.firstName;
-  let lastName = request.query.lastName;
-  response.send(`hi ${firstName} ${lastName}`);
-});
-
-app.get('/weather', (request, response, next) => {
-  // http://localhost:3001/weather?cityData=seattle
-  try {
-    console.log('hi');
-    let searchQuery = request.query.cityData;
-    let city = data.find(cityData => cityData.city_name.toLowerCase() === searchQuery.toLowerCase())
-
-    let dataMap = city.data.map(oneDay => new Forecast(oneDay));
-
-    response.status(200).send(dataMap);
-  } catch (error) {
-    next(error);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityName: '',
+      cityData: {},
+      haveCityData: false,
+      error: false,
+      errorMessage: '',
+      weatherData: [],
+      lat: '',
+      lon: ''
+    }
   }
-});
 
-app.get('*', (request, response) => {
-  response.send('The thing you are looking for doesn\'t exist');
-});
+  // WHEN DEALING WITH AXIOS YOU NEED 3 things:
+  // - 1) async
+  // - 2) await
+  // - 3) .data
+  handleCitySubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let cityUrl = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.cityName}&format=json`;
+      let city = await axios.get(cityUrl);
+      this.setState({
+        Data1: city.data[0],
+        error: false,
+        haveCityData: true,
+        lat: city.data[0].lat,
+        lon: city.data[0].lon
+      });
+      this.getWeather(city.data[0].lat, city.data[0].lon);
+    }
+    catch (error) {
+      console.log('error: ', error);
+      console.log('error.message: ', error.message);
+      this.setState({
+        error: true,
+        errorMessage: `An error Occured: ${error.response.status}`
+      });
+    }
+  }
 
-//  CLASSES
+  getWeather = async (lat, lon) => {
+    try {
+      // let { lat, lon } = this.state.Data1;
+      let weatherUrl = `${process.env.REACT_APP_SERVER}/weather?cityData=${this.state.cityName}&lat=${lat}&lon=${lon}`;
+      let weatherResponse = await axios.get(weatherUrl);
+      // console.log(weatherResponse.data)
+      let weatherData = weatherResponse.data;
+      // let date = new Date(weatherData.dt * 1000).toLocaleDateString();
+      // let description = weatherData[0].description;
+      this.setState({
+        weatherData
+      })
+      // console.log('Date:', date);
+      // console.log('Description:', description);
+    } catch (error) {
+      console.log('Error getting weather: ', error);
+    }
+  };
 
-class Forecast {
-  constructor(day) {
-    this.date = day.valid_date;
-    this.description = day.weather.description;
-    this.high = day.high_temp;
-    this.low = day.low_temp;
+
+  changeCityInput = (e) => {
+    this.setState({
+      cityName: e.target.value
+    });
+  }
+  render() {
+    // console.log(this.state.weatherData)
+    return (
+      <>
+        <header>
+          <h1>Data from an API</h1>
+          <form onSubmit={this.handleCitySubmit}>
+            <label>
+              <input name="city" onChange={this.changeCityInput} />
+            </label>
+            <Button type="submit" className="button">Explore!</Button>
+          </form>
+        </header>
+        {this.state.error ? <p>{this.state.errorMessage}</p> :
+          this.state.haveCityData &&
+          <main>
+            <Card className='City p-2 h-100%' style={{ width: '75%' }}>
+              <Card.Body>
+                <Card.Img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.Data1.lat},${this.state.Data1.lon}&zoom=12`} alt="" />
+                <Card.Title>{this.state.cityName}</Card.Title>
+                <Card.Text>Lat: {this.state.Data1.lat}</Card.Text>
+                <Card.Text>Lon: {this.state.Data1.lon}</Card.Text>
+                {this.state.weatherData.length > 0 && <Weather
+                  weatherData={this.state.weatherData}
+                  cityName={this.state.cityName}
+                />}
+              </Card.Body>
+            </Card>
+            {/* {this.state.weatherData ? (
+              this.state.weatherData.map(day => (
+                <h1>{day.date}</h1>
+              ))
+            ) : (
+              <p>loading weather data</p>
+            )
+            } */}
+          </main>
+        }
+      </>
+    );
   }
 }
 
-// LISTEN 
-
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+export default App;
